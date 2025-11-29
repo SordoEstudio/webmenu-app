a1bd6b04a456d110b2a7c20852c812687644547f
Pablo Albera <pabloalbera23@gmail.com>
2025-11-29 19:07:54 -0300

    hotfix: productdata.json delete import

--- components/ProductDetailComponent.jsx
+++ components/ProductDetailComponent.jsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import { Box, Typography, useTheme, Avatar } from "@mui/material";
 import { formatPrice } from "../utils/formatters";
 import ImageModal from "@/components/ImageModal";
-import productData from "@/data/productData.json";
 const ProductDetailComponent = ({ product }) => {
 
  /*    product =product?product:productData; */
